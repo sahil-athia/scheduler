23bd1d41122c4b837a7b22ef7fc7c05c2e44b815
Sahil Athia <sahilathia918@gmail.com>
2020-12-14 14:49:23 -0500

    test basic mock functions and expectations

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -15,5 +15,19 @@ describe("Appointment Testing", () => {
     render(<Appointment/>)
   })  
   // with jest we can use both 'it' and 'test'
-  
+
+  it("calls mock, with particular values", () => {
+    const mock = jest.fn();
+    mock("hello")
+    expect(mock).toHaveBeenCalledWith("hello");
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
+
+  it("returns mock with particular values", () => {
+    const mock = jest.fn((a, b) => a * b);
+    mock(2, 5)
+    expect(mock).toHaveBeenCalledTimes(1);
+    expect(mock).toHaveReturnedWith(10)
+  });
+  // using a mock function
 })
