3d0682ecefe0446003fc23c0db01f56d30f830ac
Sahil Athia <sahilathia918@gmail.com>
2020-12-18 12:34:49 -0500

    remove hard coded api requests and replaced with env vars

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { DayList } from './DayList.js'
 import Appointment from './Appointment/index';
 import DayList from './DayList';
 import "components/Application.scss";
@@ -14,7 +13,6 @@ export default function Application(props) {
     cancelInterview
   } = useApplicationData();
   
-  // const setDays = days => setState(prev => ({ ...prev, days }));
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const dailyInterviewers = getInterviewersForDay(state, state.day);
 
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,18 +1,17 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
-
 export default function useApplicationData() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments:{},
   })
-
+  
   useEffect(() => {
     Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers"),
+      axios.get(process.env.REACT_APP_GET_DAYS),
+      axios.get(process.env.REACT_APP_GET_APPOINTMENTS),
+      axios.get(process.env.REACT_APP_GET_INTERVIEWERS),
     ])
     .then(all => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
